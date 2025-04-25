374fcae2cc04f7b90bde8fd82d19e72798350254
Parthiv-MEV <35805472+Parthiv-MEV@users.noreply.github.com>
2025-04-25 20:38:19 +0530

    Delete ref_code/midsem directory

--- ref_code/midsem/UNIX-2.sh
+++
@@ -1,3 +0,0 @@
-mkdir ./secured_logs
-cp `find ./vought_logs/| grep -E "*.txt"` ./secured_logs/
-find ./ | grep `ls -Slh ./secured_logs/ | head -n 2 | tail -n 1 | cut -d " " -f 9` | grep -E "^\./vought_logs/" > biggest_secret.txt
